fb3b2254cb94cba663b15e478c82baf74a419566
Raphael Leveque <raphaellev1102@gmail.com>
2021-05-23 00:05:37 -0300

    Exercício de ICC1: Keyboardcat

---
+++ icc1/keyboardcat/keyboardcat.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main()
+{
+    char ordem_teclas[49];
+    int caracteres_especiais[2];
+
+    for (int i = 0; i < 49; i++)
+    {
+        scanf("%c", &ordem_teclas[i]);
+        switch (ordem_teclas[i])
+        {
+        case '_':
+            caracteres_especiais[0] = i;
+            break;
+        case 'E':
+            caracteres_especiais[1] = i;
+            break;
+        }
+        getchar();
+    }
+    int tamanho;
+    scanf("%d", &tamanho);
+    int mensagem[tamanho];
+
+    for (int i = 0; i < tamanho; i++)
+    {
+        scanf("%d", &mensagem[i]);
+        getchar();
+    }
+    
+ 
+    for (int i = 0; i < tamanho; i++)
+    {
+        if (caracteres_especiais[0] == mensagem[i])
+        {
+            printf(" ");
+        }
+        else if (caracteres_especiais[1] == mensagem[i])
+        {
+            printf("\n");
+        }
+        else
+        {
+        printf("%c", ordem_teclas[mensagem[i]]);
+        }
+    }
+      
+    return 0;
+}
