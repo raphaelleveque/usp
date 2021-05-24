a479c1fec8a6f98c23f0490b71c03f65d44cec7b
Raphael Leveque <raphaellev1102@gmail.com>
2021-05-24 11:00:56 -0300

    Exercício do GEMA: Palíndromos

--- extensoes/gema/codeforces/aula2/palindromos/palindromos.cpp
+++ extensoes/gema/codeforces/aula2/palindromos/palindromos.cpp
@@ -13,20 +13,17 @@ int main()
         cin >> palindromo[i];
     }
     
-
-
+    
+    int j = n - 1;
     if (n % 2 == 0)
     {
         for (int i = 0; i < n/2; i++)
         {
-            for (int j = n; j > n/2; j--)
+            if (palindromo[i] != palindromo[j])
             {
-                if (palindromo[i] != palindromo[j])
-                {
-                    soma++;
-                }
-                
+                soma++;
             }
+            j--;  
             
         }
     }
@@ -34,15 +31,11 @@ int main()
     {
         for (int i = 0; i < (n-1)/2; i++)
         {
-            for (int j = n-1; j > (n-1)/2; j--)
+            if (palindromo[i] != palindromo[j])
             {
-                if (palindromo[i] != palindromo[j])
-                {
-                    soma++;
-                }
-                
+                soma++;
             }
-            
+            j--;
         }
     }
     cout << soma;
