5d85db5f50d649c87fd89ce503084d848fd68de9
Raphael Leveque <raphaellev1102@gmail.com>
2021-05-24 08:49:23 -0300

    Exercício do GEMA: Cavernoso versão fakhoury

--- extensoes/gema/codeforces/aula2/cavernoso/cavernoso-fakhoury.cpp
+++ extensoes/gema/codeforces/aula2/cavernoso/cavernoso-fakhoury.cpp
@@ -1,9 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int frequencia[1005];
+
 int main()
 {
-    int n, frequencia[1005];
+    int n;
     cin >> n;
 
     for (int i = 0; i < n; i++)
@@ -12,7 +14,17 @@ int main()
         cin >> pipeline;
         int tamanho = pipeline.size();
         frequencia[tamanho]++;
+        // isso fará o seguinte: se o tamanho da string for de 8 caracteres, o vetor frequencia somará 1 na posição 8 
     }
-    cout << max_element(frequencia, frequencia + 1000);
+    // agora só precisamos saber qual a posição do maior número desse vetor
+    int answer = 0;
 
+    for (int i = 0; i < 1000; i++)
+    {
+        if (frequencia[i] > frequencia[answer])
+        {
+            answer = i;
+        }
+    }  
+    cout << answer << endl;
 }
