049982801a7e320501e1b7bf9a86c32a2a2ddb39
Raphael Leveque <raphaellev1102@gmail.com>
2021-05-27 23:15:58 -0300

    Exercícios de Lab

---
+++ lab-de-icc1/gato-obeso/gato-obeso.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int bolinha, racao;
+    scanf("%d %d", &bolinha, &racao);
+
+    if (/* condition */)
+    {
+        /* code */
+    }
+    
+    
+
+    return 0;
+}
