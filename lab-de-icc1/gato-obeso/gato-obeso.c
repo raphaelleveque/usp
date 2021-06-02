aa421a2a0f8f679d1d300f6456be92196c1dae44
Raphael Leveque <raphaellev1102@gmail.com>
2021-06-02 17:42:12 -0300

    Exercício de Lab: Gato Obeso

--- lab-de-icc1/gato-obeso/gato-obeso.c
+++ lab-de-icc1/gato-obeso/gato-obeso.c
@@ -2,13 +2,21 @@
 
 int main()
 {
-    int bolinha, racao;
+    int bolinha, racao, resultado;
     scanf("%d %d", &bolinha, &racao);
 
-    if (/* condition */)
+    resultado = (bolinha * 5) - (racao * 3);
+
+    if (resultado >= 0 && resultado < 30 )
     {
-        /* code */
+        printf("B\n");
     }
+    else if (resultado < 0)
+    {
+        printf("R\n");
+    }
+    else
+        printf("P\n");
     
     
 
