d8b885e5e154221a5e2dec82f1340623c9a86009
Raphael Leveque <raphaellev1102@gmail.com>
2021-06-26 10:54:56 -0300

    Exercício de ICC1: Segmentação de Memória

---
+++ icc1/Bloco2/livro-corrompido/livro-corrompido.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+char *read_line()
+{
+
+
+}
+
+int main()
+{
+    int numero_linhas;
+    scanf("%d", &numero_linhas);
+
+    char frases[numero_linhas][1];
+
+    for (int i = 0; i < numero_linhas; i++)
+    {
+    }
+    
+
+    return 0;
+}
