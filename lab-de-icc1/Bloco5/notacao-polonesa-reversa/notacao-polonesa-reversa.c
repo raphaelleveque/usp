abd302f26f6fa853bd3b1b95b36832fa2c1f1eec
Raphael Leveque <raphaellev1102@gmail.com>
2021-07-13 18:54:59 -0300

    Exercício de Lab modificado: Notação Polonesa Reversa

--- lab-de-icc1/Bloco5/notacao-polonesa-reversa/notacao-polonesa-reversa.c
+++ lab-de-icc1/Bloco5/notacao-polonesa-reversa/notacao-polonesa-reversa.c
@@ -4,50 +4,64 @@
 int main()
 {
     char numberOrOperator;
-    double stack[15] = {};
-    int i = 0;
+    double *stack = malloc(2 * sizeof(double));
+    double *stackPointer;
+    int stackSize = 0;
     
     while ((numberOrOperator = getchar()) != EOF)
     {
         if (numberOrOperator >= '0' && numberOrOperator <= '9')
         {
             ungetc(numberOrOperator, stdin);
-            scanf("%lf", &stack[i]);
-            i++;
+            scanf("%lf", &stack[stackSize]);
+            stackSize++;
+            if (stackSize % 2 == 1)
+            {
+                stack = realloc(stack, (stackSize * 2 + 1) * sizeof(double));
+            }
+            stackPointer = &stack[stackSize];
         }
         else if (numberOrOperator == ' ' );
         else
         {
             if (numberOrOperator == '+')
             {
-                i -= 2;
-                stack[i] = stack[i] + stack[i + 1];
-                i++;
+                stackPointer -= 2;
+                *stackPointer = *stackPointer + *(stackPointer + 1);
+                stackPointer ++;
+                stack = realloc(stack, stackSize * sizeof(double));
+                stackSize--;
             }
             else if (numberOrOperator == '-')
             {
-                i -= 2;
-                stack[i] = stack[i] - stack[i + 1];
-                i++;
+                stackPointer -= 2;
+                *stackPointer = *stackPointer - *(stackPointer + 1);
+                stackPointer ++;
+                stack = realloc(stack, stackSize * sizeof(double));
+                stackSize--;
             }
             else if (numberOrOperator == '*')
             {
-                i -= 2;
-                stack[i] = stack[i] * stack[i + 1];
-                i++;
+                stackPointer -= 2;
+                *stackPointer = *stackPointer * *(stackPointer + 1);
+                stackPointer ++;
+                stack = realloc(stack, stackSize * sizeof(double));
+                stackSize--;
             }
             else if (numberOrOperator == '/')
             {
-                i -= 2;
-                stack[i] = stack[i] / stack[i + 1];
-                i++;
+                stackPointer -= 2;
+                *stackPointer = *stackPointer / *(stackPointer + 1);
+                stackPointer ++;
+                stack = realloc(stack, stackSize * sizeof(double));
+                stackSize--;
             }
 
         }
         
         
     }
-    printf("Resultado: %.6lf", stack[i - 1]);
+    printf("Resultado: %.6lf", stack[stackSize - 1]);
 
     return 0;
 }
