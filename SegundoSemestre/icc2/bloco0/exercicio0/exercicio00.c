a8f0993c410b8165293bc37db521f9199535a39e
Raphael Leveque <raphaellev1102@gmail.com>
2021-08-20 20:50:54 -0300

    Exercício de ICC2: Exercício 0.0

--- SegundoSemestre/icc2/bloco0/exercicio0/exercicio00.c
+++ SegundoSemestre/icc2/bloco0/exercicio0/exercicio00.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+void checkFrequency(int *arrayPosition, int array[*arrayPosition], int *amountSameValue, bool *duplicateValues)
+{
+for (int i = 0; i < *arrayPosition; i++)
+    {
+        for (int j = i + 1; j < *arrayPosition; j++)
+        {
+            if (array[i] == array[j])
+            {
+                (*amountSameValue)++;
+            }
+        }
+        
+        if (i > 0)
+        {
+            for (int j = i - 1; j > -1; j--)
+            {
+                *duplicateValues = (array[i] == array[j]);
+                if (*duplicateValues)
+                {
+                    break;
+                }
+            }  
+        }
+        if (!(*duplicateValues))
+        {   
+            printf("%d (%d)\n", array[i], *amountSameValue);
+        }
+        *amountSameValue = 1;
+    }
+}
+
 int main()
 {
     int array[100] = {}, arrayPosition = 0;
@@ -12,34 +43,8 @@ int main()
 
     bool duplicateValues = false;
     int amountSameValue = 1;
-    for (int i = 0; i < arrayPosition; i++)
-    {
-        for (int j = i + 1; j < arrayPosition; j++)
-        {
-            if (array[i] == array[j])
-            {
-                amountSameValue++;
-            }
-        }
-        
-        if (i > 0)
-        {
-            for (int j = i - 1; j > -1; j--)
-            {
-                duplicateValues = (array[i] == array[j]);
-                if (duplicateValues)
-                {
-                    break;
-                }
-            }  
-        }
-        if (!duplicateValues)
-        {   
-            printf("%d (%d)\n", array[i], amountSameValue);
-        }
-        amountSameValue = 1;
-    }
-    
+
+    checkFrequency(&arrayPosition, array, &amountSameValue, &duplicateValues);
 
     return 0;
 }
