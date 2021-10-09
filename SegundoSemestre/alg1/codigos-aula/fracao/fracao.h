c41400081106c52ebc552e7c2b96aa3e29e2fd5f
Raphael Leveque <raphaellev1102@gmail.com>
2021-10-08 21:17:25 -0300

    removendo codigos aula alg1

--- SegundoSemestre/alg1/codigos-aula/fracao/fracao.h
+++
@@ -1,9 +0,0 @@
-typedef struct
-{
-    int num, den;
-} fracao_t;
-
-fracao_t *criar(int num, int den);
-void imprimir(fracao_t *f);
-void destruir(fracao_t *f);
-fracao_t *soma(fracao_t *f1, fracao_t *f2);
