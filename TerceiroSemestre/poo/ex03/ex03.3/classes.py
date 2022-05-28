f24325223ffe7301e86cc746de3af2ffef76f830
Raphael Leveque <raphaellev1102@gmail.com>
2022-05-28 00:31:14 -0300

    Exercicio 3.3 de poo

--- TerceiroSemestre/poo/ex03/ex03.3/classes.py
+++ TerceiroSemestre/poo/ex03/ex03.3/classes.py
@@ -1,42 +1,56 @@
 
 class FigurasGeometricas:
-    def __init__(self, figura) -> None:
+    def __init__(self, figura, cor, preenchido) -> None:
         self.figura = figura
+        self.cor = cor
+        self.preenchido = preenchido
+
+    def __str__(self, area, perimetro) -> str:
+        return f"\n\nO {self.figura} possui Area = {area:.1f} e Perímetro = {perimetro:.1f}.\nAlém disso, o {self.figura} {'' if self.preenchido else 'não '}está preenchido{f' com a cor {self.cor}' if self.preenchido else ''}.\n\n"
 
 
 class Circulo(FigurasGeometricas):
-    PI = 3.1415
-    def __init__(self, raio) -> None:
+    PI = 3.14
+    def __init__(self, raio, cor, preenchido) -> None:
         self.raio = raio
-        super().__init__("Círculo")
+        super().__init__("Círculo", cor, preenchido)
 
     def calcular_area(self):
         return Circulo.PI * (self.raio ** 2)
 
     def calcular_perimetro(self):
         return 2 * Circulo.PI * self.raio
+    
+    def __str__(self) -> str:
+        return super().__str__(self.calcular_area(), self.calcular_perimetro())
 
 
 class Quadrado(FigurasGeometricas):
-    def __init__(self, lado) -> None:
+    def __init__(self, lado, cor, preenchido) -> None:
         self.lado = lado
-        super().__init__("Quadrado")
+        super().__init__("Quadrado", cor, preenchido)
 
     def calcular_area(self):
         return self.lado ** 2
 
     def calcular_perimetro(self):
         return 4 * self.lado
+    
+    def __str__(self) -> str:
+        return super().__str__(self.calcular_area(), self.calcular_perimetro())
 
 
 class Retangulo(FigurasGeometricas):
-    def __init__(self, lado1, lado2) -> None:
+    def __init__(self, lado1, lado2, cor, preenchido) -> None:
         self.lado1 = lado1
         self.lado2 = lado2
-        super().__init__("Retângulo")
+        super().__init__("Retângulo", cor, preenchido)
 
     def calcular_area(self):
         return self.lado1 * self.lado2
 
     def calcular_perimetro(self):
         return 2 * (self.lado1 + self.lado2)
+    
+    def __str__(self) -> str:
+        return super().__str__(self.calcular_area(), self.calcular_perimetro())
--- TerceiroSemestre/poo/ex03/ex03.3/main.py
+++ TerceiroSemestre/poo/ex03/ex03.3/main.py
@@ -1,6 +1,55 @@
 from classes import *
 
+def main():
+    operacao = -1
+    figura:FigurasGeometricas
+    while operacao:
+        print("Para adicionar um círculo, digite 1")
+        print("Para adicionar um quadrado, digite 2")
+        print("Para adicionar um retângulo, digite 3")
+        print("Para encerrar o programa, digite 0")
+        operacao = int(input())
+
+        if operacao > 3 and operacao < 0:
+            exit("Comando inválido")
+        
+        if operacao == 0:
+            break
+
+        if operacao == 1:
+            cor = input("Qual a cor do círculo? ")
+            preenchida = input("O círculo está preenchido (s/n)? ")
+            if preenchida == "s":
+                preenchida = True
+            elif preenchida == "n":
+                preenchida = False
+            raio = int(input("Qual o raio do círculo? "))
+            figura = Circulo(raio, cor, preenchida)
+        
+        elif operacao == 2:
+            cor = input("Qual a cor do quadrado? ")
+            preenchida = input("O quadrado está preenchido (s/n)? ")
+            if preenchida == "s":
+                preenchida = True
+            elif preenchida == "n":
+                preenchida = False
+            lado = int(input("Qual o lado do quadrado? "))
+            figura = Quadrado(lado, cor, preenchida)
+        
+        elif operacao == 3:
+            cor = input("Qual a cor do retângulo? ")
+            preenchida = input("O retângulo está preenchido (s/n)? ")
+            if preenchida == "s":
+                preenchida = True
+            elif preenchida == "n":
+                preenchida = False
+            lado1, lado2 = map(int, input("Quais os lados do retângulo? ").split())
+            figura = Retangulo(lado1, lado2, cor, preenchida)
+
+        print(figura)
 
 
 
 
+if __name__ == "__main__":
+    main()
