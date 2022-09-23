3381f94ead255149d8b221055f1db59fde5af324
Raphael Leveque <raphaellev1102@gmail.com>
2022-09-23 14:19:04 -0300

    aula 2 icd

---
+++ QuartoSemestre/algs-avancados/backtracking-2/casos/README.txt
@@ -0,0 +1,3 @@
+Arquivo zip gerado em: 19/09/2022 16:13:27 
+Este arquivo contém os casos de teste cadastrados até o momento, disponibilizado pelo professor aos alunos.
+Exercício: BKT 2
---
+++ QuartoSemestre/algs-avancados/backtracking-2/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int k;
+    cin >> k;
+
+    while (k--) {
+        vector<vector<int>> chessboard(8, vector<int>(8));
+
+
+
+
+    }
+
+
+
+    return 0;
+}
